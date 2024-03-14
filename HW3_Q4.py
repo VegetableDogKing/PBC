509b01cdfa08e61da5c02ed355481f163c06b88d
VegetableDogKing <vincent97295@gmail.com>
2024-03-15 05:08:10 +0800

    Q4

---
+++ HW3_Q4.py
@@ -0,0 +1,37 @@
+tasks_count = int(input())
+order_count = tasks_count + 1
+
+process_time = input().split(',')
+deadline = input().split(',')
+order_origin = input().split(',')
+
+for i in range(tasks_count):
+    process_time[i] = int(process_time[i])
+    deadline[i] = int(deadline[i])
+    order_origin[i] = int(order_origin[i])
+
+order = []
+order.append(order_origin.copy())
+for i in range(tasks_count):
+    order.append(order_origin.copy())
+    tmp = order[i+1][i]
+    order[i+1][i] = order[i+1][(i+1) % tasks_count]
+    order[i+1][(i+1) % tasks_count] = tmp
+
+delay_tol = [0] * order_count
+time = [0] * order_count
+for i in range(tasks_count):
+    for j in range(order_count):
+        time[j] += process_time[order[j][i]-1]
+        delay = time[j] - deadline[order[j][i]-1]
+        if delay > 0:
+            delay_tol[j] += delay
+
+index = 0
+delay_min = delay_tol[0]
+for i in range(1, order_count):
+    if delay_tol[i] < delay_min:
+        index = i
+        delay_min = delay_tol[i]
+
+print(delay_min)
